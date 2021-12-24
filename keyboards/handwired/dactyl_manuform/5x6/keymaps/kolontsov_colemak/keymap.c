027b7ea2d93f848537fee793ca10f087bcc1e0db
Vadim Kolontsov <vadim@kolontsov.com>
2021-12-25 01:07:38 +0300

    add ALFRED, fix arrows

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/kolontsov_colemak/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/kolontsov_colemak/keymap.c
@@ -13,6 +13,7 @@
 #define ADJUST MO(_ADJUST)
 
 #define LANG LGUI(KC_SPC)
+#define ALFRED HYPR(KC_QUOT)
 
 // https://precondition.github.io/home-row-mods#cags
 
@@ -37,8 +38,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_D,   KC_V,        KC_K, KC_H,   KC_COMM, KC_DOT, KC_SLSH, KC_BSLASH,
                       KC_GRV, KC_HYPR,                                   KC_LBRC, KC_RBRC,
                                       KC_LGUI, _______,    _______, KC_BSPC,
-                                        KC_LALT, LOWER,    RAISE, KC_RGUI,
-                                        LANG, KC_SPC,      KC_ENT, _______
+                                        KC_LALT, LOWER,    RAISE,   KC_RGUI,
+                                        LANG, KC_SPC,      KC_ENT,  ALFRED
   ),
 
   [_LOWER] = LAYOUT_5x6(
@@ -56,8 +57,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RAISE] = LAYOUT_5x6(
      _______, KC_F1,   KC_F2,   KC_F3,  KC_F4,     KC_F5,     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
-     _______, _______, _______, _______, _______, _______,    _______, _______, _______, KC_UP,   _______, _______,
-     _______, _______, _______, _______, _______, _______,    _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT,_______,
+     _______, _______, _______, _______, _______, _______,    _______, _______, KC_UP,   _______, _______, _______,
+     _______, _______, _______, _______, _______, _______,    _______, KC_LEFT, KC_DOWN, KC_RIGHT,_______, _______,
      _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______,
                        _______, _______,                                        _______, _______,
                                          _______, _______,    _______, _______,
